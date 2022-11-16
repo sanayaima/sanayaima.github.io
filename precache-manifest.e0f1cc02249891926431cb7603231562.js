5533e2ca8603acd676db24849ce23c3137f85e37
RK Sanayaima Singh <rksanayai@gmail.com>
2022-11-17 00:14:08 +0530

    Updates

---
+++ precache-manifest.e0f1cc02249891926431cb7603231562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6e6605490f2e2046092",
+    "url": "/static/css/main.eb4901c2.chunk.css"
+  },
+  {
+    "revision": "c6e6605490f2e2046092",
+    "url": "/static/js/main.7a1f5601.chunk.js"
+  },
+  {
+    "revision": "99798f2f2cd34153bd78",
+    "url": "/static/js/runtime~main.1ae3213d.js"
+  },
+  {
+    "revision": "c58cb75a7feaf5003ac1",
+    "url": "/static/js/2.e6637c1e.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e08bd4beac581b117bb98659e485aa4c",
+    "url": "/index.html"
+  }
+];
